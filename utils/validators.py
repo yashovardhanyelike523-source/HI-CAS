85dac911e03f6942847e3f32aad022a33f493c10
yashovardhan <yashovardhanyelike523@gmail.com>
2026-04-25 10:52:36 +0530

    first commit
    Removed comments from the validate_expiry function to clean up the code.

--- utils/validators.py
+++ utils/validators.py
@@ -3,10 +3,7 @@ from datetime import datetime
 
 def validate_expiry(date_text):
     try:
-        # Check format and valid date
         datetime.strptime(date_text, "%d-%m-%Y")
-
-        # Ensure year is exactly 4 digits
         year = date_text.split("-")[2]
         if len(year) != 4:
             return False
